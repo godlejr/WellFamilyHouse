61e547217dac01319041de6001c5888db45df9db
dongjoo-kim <godlejr@gmail.com>
2017-06-13 17:00:22 +0900

    gid add

--- app/src/main/java/com/demand/well_family/well_family/notification/falldiagnosis/presenter/impl/NotificationFallDiagnosisStoryPresenterImpl.java
+++ app/src/main/java/com/demand/well_family/well_family/notification/falldiagnosis/presenter/impl/NotificationFallDiagnosisStoryPresenterImpl.java
@@ -2,12 +2,20 @@ package com.demand.well_family.well_family.notification.falldiagnosis.presenter.
 
 import android.content.Context;
 
+import com.demand.well_family.well_family.dto.CommentInfo;
+import com.demand.well_family.well_family.dto.EnvironmentEvaluationCategory;
+import com.demand.well_family.well_family.dto.EnvironmentPhoto;
+import com.demand.well_family.well_family.dto.FallDiagnosisContentCategory;
 import com.demand.well_family.well_family.dto.FallDiagnosisStory;
+import com.demand.well_family.well_family.dto.FallDiagnosisStoryComment;
+import com.demand.well_family.well_family.dto.PhysicalEvaluationScore;
 import com.demand.well_family.well_family.notification.falldiagnosis.interactor.NotificationFallDiagnosisStoryInteractor;
 import com.demand.well_family.well_family.notification.falldiagnosis.interactor.impl.NotificationFallDiagnosisStoryInteractorImpl;
 import com.demand.well_family.well_family.notification.falldiagnosis.presenter.NotificationFallDiagnosisStoryPresenter;
 import com.demand.well_family.well_family.notification.falldiagnosis.view.NotificationFallDiagnosisStoryView;
 import com.demand.well_family.well_family.util.APIErrorUtil;
+
+import java.util.ArrayList;
 
 /**
  * Created by ㅇㅇ on 2017-06-13.
@@ -30,6 +38,56 @@ public class NotificationFallDiagnosisStoryPresenterImpl implements Notification
 
     @Override
     public void onLoadData() {
+
+    }
+
+    @Override
+    public void onSuccessSetContentLikeUp() {
+
+    }
+
+    @Override
+    public void onSuccessSetContentLikeDown() {
+
+    }
+
+    @Override
+    public void onSuccessGetPhysicalEvaluationScore(PhysicalEvaluationScore physicalEvaluationScore) {
+
+    }
+
+    @Override
+    public void onSuccessGetSelfDiagnosisList(ArrayList<FallDiagnosisContentCategory> fallDiagnosisContentCategoryList) {
+
+    }
+
+    @Override
+    public void onSuccessGetEnvironmentPhoto(ArrayList<EnvironmentPhoto> environmentPhotoList) {
+
+    }
+
+    @Override
+    public void onSuccessGetEnvironmentEvaluationList(ArrayList<EnvironmentEvaluationCategory> environmentEvaluationCategoryList) {
+
+    }
+
+    @Override
+    public void onSuccessGetFallDiagnosisStoryCommentList(ArrayList<CommentInfo> commentInfoList) {
+
+    }
+
+    @Override
+    public void onSuccessSetFallDiagnosisStoryComment(FallDiagnosisStoryComment fallDiagnosisStoryComment) {
+
+    }
+
+    @Override
+    public void onSuccessGetFallDiagnosisStoryCommentCount(int commentCount) {
+
+    }
+
+    @Override
+    public void onSuccessGetFallDiagnosisStoryLikeCount(int likeCount) {
 
     }
 
